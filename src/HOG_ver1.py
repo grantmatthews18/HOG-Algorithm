9c37818d6bd1388566fa40054fde80f69eff6b4f
Grant Matthews <grant.d.matthews@gmail.com>
2022-09-26 10:07:23 -0500

    Creating Gradient Filters

--- src/HOG_ver1.py
+++ src/HOG_ver1.py
@@ -3,7 +3,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def get_differential_filter():
-    # To do
+    filter_x = np.array([-1,0,1], [-2,0,2], [-1,0,1])
+    filter_y = np.array([-1,-2,-1], [0,0,0], [1,2,1])
+
     return filter_x, filter_y
 
 
